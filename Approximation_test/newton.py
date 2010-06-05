880ba5f6c6f229ed6a6d773c9dad39e43104c13f
davidmenhur <davidmenhur@localhost>
2010-06-05 21:14:32 +0200

    Newton3, finally working properly.

--- Approximation_test/newton.py
+++ Approximation_test/newton.py
@@ -3,41 +3,29 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from numpy import array, dot, linalg
+from numpy import array
 from pylab import show, savefig
-from math import sqrt
 from time import time
 import create
-import psyco
 
 def raytrace(x0, v, mass):
     f=array([0,0,0])
     for point in mass:
-        r=array([x0[0]-point[0][0], x0[1]-point[0][1], x0[2]-point[0][2]])
-        r2=mag2(r)
+        r2=mag2(x0-point[0])
         M=point[1]
-        fi=M/r2
-        f+=mag(r)*fi
+        f+=M/r2
     f*=G
     v+=f*dt
-    v[2]=0
-    x0+=v*dt
-    return x0, v
+    x1=x0+v*dt
+    return x1, v
 
 def mag2(vec):
-    return dot(vec,vec)
-
-def mag(vec):
-    return linalg.norm(vec)
+    return vec*vec
 
 def read(x0, pic):                   #Eats an array and returns the correspondient pixel
     x=round(x0[0])
     y=round(x0[1])
-    if x<0 or y<0: return (1,0,0)
-    elif x>m or y>n:
-        #print x, y
-        return (0,1,0)
-        
+    if x<0 or y<0 or x>m or y>n: return (1,0,0)
     else: return pic[x][y]
 
 
@@ -58,18 +46,15 @@ dt=1
 maxit=2                             # Fast test
 
 #Iterating:
-psyco.full()
-
 print 'start'
 t0=time()
 
 for x in range(m):
     for y in range(n):
         z=0
-        pos=array([x,y,z])
         vel=(0, 0, 1)
-        
         for it in range(maxit):
+            pos=array([x,y,z])
             pos, vel=raytrace(pos,vel,lens)
         imgf[x][y]=read(pos, img)
         
