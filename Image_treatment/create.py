52104db8a4bb00757823400e9081b94ec79a352a
davidmenhur <davidmenhur@localhost>
2010-06-06 16:50:43 +0200

    Protecting create.py against bad input.

--- Image_treatment/create.py
+++ Image_treatment/create.py
@@ -2,9 +2,28 @@
 
 from numpy import array, reshape, zeros, ones
 
-def black(m, n): return zeros(n*m*3).reshape(m, n, 3)
-def white(m, n): return ones(n*m*3).reshape(m, n, 3)
+def black(m, n):
+    if type(m) in [int, float] and type(n) in [int, float]:
+        if int(m)==m and m>0 and int(n)==n and n>0: return zeros(n*m*3).reshape(m, n, 3)
+        else: raise 'TypeError. Dimensions should be positive integers.'
+    else: raise 'TypeError. Dimensions should be valid numbers.'
+    
+   
+
+def white(m, n):
+    if type(m) in [int, float] and type(n) in [int, float]:
+        if int(m)==m and m>0 and int(n)==n and n>0: return ones(n*m*3).reshape(m, n, 3)
+        else: raise 'TypeError. Dimensions should be positive integers.'
+    else: raise 'TypeError. Dimensions should be valid numbers.'
+    
+
 def color(m, n, col):
+    if type(m) in [int, float] and type(n) in [int, float]:
+        if int(m)==m and m>0 and int(n)==n and n>0:
+            pass                                            # Trivial
+        else: raise 'TypeError. Dimensions should be positive integers.'
+    else: raise 'TypeError. Dimensions should be valid numbers.'
+    
     if (type(col)!=list and type(col)!=tuple and type(col)!=numpy.ndarray):
         raise 'TypeError. Atribute color must be a list, a tuple or an array.'
     if (len(col)!=3 and len(col)!=4):
