425140543e91786bce3a21269c5ca0fe9568fe95
davidmenhur <davidmenhur@localhost>
2010-07-08 04:58:59 +0200

    A more refined one, and a cleaner script.

--- Deflection/exporter.py
+++ Deflection/exporter.py
@@ -1,19 +1,10 @@
-from __future__ import division
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
-from numpy import array, dot, linalg, asarray
 from pylab import show, savefig
-from math import sqrt, tan, asin
-from time import time
-import create
-import psyco
-
 
 img=mpimg.imread('galaxy.png')
-it=0
 
 for i in xrange(3):
     img2=img[1:30*(i+1), 20*(i+1):300] 
     plt.imshow(img2).set_interpolation('nearest')
-    savefig('section'+str(i)+'.png')
+    savefig('section'+str(i).zfill(5)+'.png')
