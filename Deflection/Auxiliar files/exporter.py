629647642dfb445772641660c4cc58003da5d354
davidmenhur <davidmenhur@localhost>
2010-07-08 19:49:56 +0200

    HD version.

--- Deflection/Auxiliar files/exporter.py
+++ Deflection/Auxiliar files/exporter.py
@@ -1,10 +1,19 @@
+from __future__ import division
+import matplotlib
+matplotlib.use('Agg')           # Backend.
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
+from numpy import array, dot, linalg, asarray
 from pylab import show, savefig
+from math import sqrt, tan, asin
+from time import time
+import psyco
 
-img=mpimg.imread('galaxy.png')
 
-for i in xrange(3):
-    img2=img[1:30*(i+1), 20*(i+1):300] 
-    plt.imshow(img2).set_interpolation('nearest')
-    savefig('section'+str(i).zfill(5)+'.png')
+img=mpimg.imread('hdf_l.png')
+
+for i in xrange(1):
+#    img2=img[1:30*(i+1), 20*(i+1):300] 
+    plt.imshow(img).set_interpolation('nearest')
+    savefig('section'+str(i).zfill(5)+'.png', dpi=205)
